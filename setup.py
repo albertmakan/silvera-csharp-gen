46422e7f036f35ce5e9772d270ee4818d1d39145
albertmakan <makanalbert@gmail.com>
2022-09-03 15:02:09 +0200

    packages in setup

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
 setup(
-    name="csharp-generator",
+    name="silvera-csharp-gen",
     version="0.0.1",
     description="C# code generator for Silvera tool",
+    packages=['csharpgen'],
     entry_points={
         "silvera_generators": ["csharp = csharpgen.generator:csharp"]
     },
